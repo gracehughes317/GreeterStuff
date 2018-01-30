2fe1412c74ea18e0ca77fdc70b2b63ebf80791cc
Unknown <gracehughes317@gmail.com>
2018-01-30 10:36:21 -0600

    Merge branch 'Constructor' into dev

---
+++ src/greeterProj/TestClass.java
@@ -0,0 +1,14 @@
+package greeterProj;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Greeter joe = new Greeter();
+		System.out.println(joe.toString());
+		
+		
+	}
+
+}
