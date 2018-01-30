b59d12672b1495cc5bda1d6cc537d38f1e372ca6
Unknown <gracehughes317@gmail.com>
2018-01-30 10:41:00 -0600

    Array created

--- src/greeterProj/TestClass.java
+++ src/greeterProj/TestClass.java
@@ -8,6 +8,18 @@ public class TestClass {
 		Greeter joe = new Greeter();
 		System.out.println(joe.toString());
 		
+		Greeter[] arr = new Greeter[5];
+		arr[0] = new Greeter("Sally",20);
+		arr[1] = new Greeter("Jamie",11);
+		arr[2]= new Greeter("Mark",23);
+		arr[3]= new Greeter("Edna",57);
+		arr[4]= joe;
+		
+		for(int c = 0; c<5; c++)
+		{
+			System.out.println(arr[c].toString());
+		}
+		
 		
 	}
 
